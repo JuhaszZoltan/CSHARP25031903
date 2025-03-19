aaef6c4e2a0353817466859055eaab244e413d36
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2025-03-19 17:57:30 +0100

    Add project files.

---
+++ Dog.cs
@@ -0,0 +1,11 @@
+﻿namespace CSHARP25031903;
+
+public class Dog
+{
+    public string Name { get; set; }
+    public DateTime Birth { get; set; }
+    public string Breed { get; set; }
+    public bool Sex { get; set; }
+    public float Weight { get; set; }
+    public int Age => DateTime.Now.Year - Birth.Year;
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using CSHARP25031903;
+
+List<Dog> dogs = 
+[
+    new() // 001
+    {
+        Name = "Thomas Edison",
+        Breed = "mixed breed",
+        Weight = 22.5F,
+        Sex = true,
+        Birth = DateTime.Parse("2006-05-15"),
+    },
+    new() // 002
+    {
+        Name = "Frakk",
+        Breed = "vizsla",
+        Weight = 30F,
+        Sex = true,
+        Birth = DateTime.Parse("1972-12-23"),
+    },
+    new() // 003
+    {
+        Name = "Rex",
+        Breed = "german sepherd",
+        Weight = 35F,
+        Sex = true,
+        Birth = DateTime.Parse("1994-11-10"),
+    },
+    new() // 004
+    {
+        Name = "Szotyi",
+        Breed = "tibetian terrier",
+        Weight = 15.5F,
+        Sex = false,
+        Birth = DateTime.Parse("2021-04-02"),
+    },
+    new() // 005
+    {
+        Name = "Tappancs",
+        Breed = "mixed breed",
+        Weight = 18.75F,
+        Sex = true,
+        Birth = DateTime.Parse("1992-07-21"),
+    },
+    new() // 006
+    {
+        Name = "Buksi",
+        Breed = "dachshund",
+        Weight = 4F,
+        Sex = false,
+        Birth = DateTime.Parse("2024-12-24"),
+    },
+    new() // 007
+    {
+        Name = "Mr. Wick",
+        Breed = "winter white dwarf hamster",
+        Weight = 0.1F,
+        Sex = true,
+        Birth = DateTime.Parse("2023-06-12"),
+    },
+];
